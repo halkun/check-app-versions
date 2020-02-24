ecddbc4dd65935968a4836f0a59db56d2807570e
halkun <halkun@users.noreply.github.com>
2020-02-23 19:28:26 -0800

    Created dictionary file with apps info

---
+++ apps_info.py
@@ -0,0 +1,46 @@
+# VC Redist: 0.27.0
+# 7zip: 19.00
+# Audacity: 2.3.3
+# Calibre: 4.11.2
+# cmder: 1.3.14
+# CrystalDiskInfo: 8.4.0
+# CrystalDiskInfo: 7.0.0g
+# Dropbox: 91.4.548
+# EqualizerAPO: 1.2.1
+# Peace Equalizer: 2019-10-14
+# Search Everything: 1.4.1.935
+# Exiftool: 11.88
+# FastStone: 7.4
+# FastStone: 20200223-90913ab
+# Firefox: 73.0.1
+# Git: 2.25.1
+# Greenshot: 1.2.10.6
+# Hashtab: 6.0.0.34
+# HWInfo: 6.22
+# iTunes: 12.10.4
+# K-Lite Codec: 15.3.5
+# MKVToolnix: 43.0.0
+# OBS: 24.0.3
+# Open Shell: 4.4.142
+# Sublime Text: 3210
+# Visual Studio Code: 1.42
+# WinSCP: 5.17
+# Python: 3.8.1
+# Rufus: 3.8
+# Google Play Music Desktop: 4.7.1
+# Java: Version 8 Update 241
+
+visual_c = {
+    "app_name": 'Visual C Redistributable'
+    "app_local": '0.27.0',
+    "app_link": 'https://github.com/abbodi1406/vcredist/releases',
+    'app_ver_xpath': '//div[@class="f1 flex-auto min-width-0 text-normal"]/a/text()'
+}
+
+seven_zip = {
+    "app_name": '7zip'
+    "app_local": '19.00',
+    "app_link": 'https://www.7-zip.org/download.html',
+    'app_ver_xpath': '//tr/td[2]/p[1]/b/text()'
+}
+
