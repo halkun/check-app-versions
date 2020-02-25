4726bec11a8a0344377aa335131185ea1b8838dd
halkun <halkun@users.noreply.github.com>
2020-02-24 23:41:50 -0800

    Combined app check functions into one function Used if elif and else statements with specific rules for version string

--- check_apps.py
+++ check_apps.py
@@ -3,6 +3,7 @@ import openpyxl
 import requests
 
 
+# extract raw version data using the website's html xpath
 def scrape_app_version(app_link, ver_xpath):
     my_headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0"}
 
@@ -12,258 +13,11 @@ def scrape_app_version(app_link, ver_xpath):
 
     return app_version
 
-def seven_zip_check():
-    zip_link = "https://www.7-zip.org/download.html"
-    zip_ver_xpath = '//tr/td[2]/p[1]/b/text()'
-    zip_ver_list = scrape_app_version(zip_link, zip_ver_xpath)
-    zip_ver = zip_ver_list[0].split(' ')[2]
-
-    print("7zip: {}".format(zip_ver))
-
-def vc_check():
-    vc_link = 'https://github.com/abbodi1406/vcredist/releases'
-    vc_ver_xpath = '//div[@class="f1 flex-auto min-width-0 text-normal"]/a/text()'
-    vc_ver_list = scrape_app_version(vc_link, vc_ver_xpath)
-    vc_ver = vc_ver_list[0][1:]
-
-    print("VC Redist: {}".format(vc_ver))
-
-def audacity_check():
-    audacity_link = 'https://www.fosshub.com/Audacity.html'
-    audacity_xpath = '//dd[@itemprop="softwareVersion"]/text()'
-    audacity_ver_list = scrape_app_version(audacity_link, audacity_xpath)
-    audacity_ver = str(audacity_ver_list[0])
-
-    print("Audacity: {}".format(audacity_ver))
-
-def calibre_check():
-    calibre_link = 'https://calibre-ebook.com/download_windows'
-    calibre_xpath = '//div[@id="download_block"]/div/text()'
-    calibre_ver_list = scrape_app_version(calibre_link, calibre_xpath)
-    calibre_ver = calibre_ver_list[1].split(' ')[1]
-
-    print("Calibre: {}".format(calibre_ver))
-
-def cmder_check():
-    cmder_link = 'https://github.com/cmderdev/cmder/releases'
-    cmder_ver_xpath = '//div[@class="f1 flex-auto min-width-0 text-normal"]/a/text()'
-    cmder_ver_list = scrape_app_version(cmder_link, cmder_ver_xpath)
-    cmder_ver = cmder_ver_list[0][1:]
-
-    print("cmder: {}".format(cmder_ver))
-
-def crystaldiskinfo_check():
-    crystaldiskinfo_link = 'https://crystalmark.info/en/download'
-    crystaldiskinfo_ver_xpath = '//div[@class="entry entry-content"]/h2[2]/text()'
-    crystaldiskinfo_ver_list = scrape_app_version(crystaldiskinfo_link, crystaldiskinfo_ver_xpath)
-    crystaldiskinfo_ver = crystaldiskinfo_ver_list[0].split(' ')[2]
-
-    print("CrystalDiskInfo: {}".format(crystaldiskinfo_ver))
-
-def crystaldiskmark_check():
-    crystaldiskinfo_link = 'https://crystalmark.info/en/download'
-    crystaldiskinfo_ver_xpath = '//div[@class="entry entry-content"]/h2[3]/text()'
-    crystaldiskinfo_ver_list = scrape_app_version(crystaldiskinfo_link, crystaldiskinfo_ver_xpath)
-    crystaldiskinfo_ver = crystaldiskinfo_ver_list[0].split(' ')[3]
-
-    print("CrystalDiskMark: {}".format(crystaldiskinfo_ver))
-
-def dropbox_check():
-    dropbox_link = 'https://en.wikipedia.org/wiki/Dropbox_(service)'
-    dropbox_ver_xpath = '//table[@class="infobox vevent"]/tbody/tr/td//text()'
-    dropbox_ver_list = scrape_app_version(dropbox_link, dropbox_ver_xpath)
-    dropbox_ver = dropbox_ver_list[7].split(' ')[0]
-
-    print("Dropbox: {}".format(dropbox_ver))
-
-def equalizer_apo_check():
-    equalizer_apo_link = 'https://sourceforge.net/projects/equalizerapo/files'
-    equalizer_apo_ver_xpath = '//tr[@class="folder "]/th/a/span/text()'
-    equalizer_apo_ver_list = scrape_app_version(equalizer_apo_link, equalizer_apo_ver_xpath)
-    equalizer_apo_ver = equalizer_apo_ver_list[0]
-
-    print("EqualizerAPO: {}".format(equalizer_apo_ver))
-
-def firefox_check():
-    firefox_link = 'https://en.wikipedia.org/wiki/Firefox'
-    firefox_ver_xpath = '//table[@class="infobox vevent"]/tbody/tr/td//text()'
-    firefox_ver_list = scrape_app_version(firefox_link, firefox_ver_xpath)
-    firefox_ver = firefox_ver_list[21].split(' ')[0]
-
-    print("Firefox: {}".format(firefox_ver))
-
-def peace_equalizer_check():
-    peace_equalizer_link = 'https://sourceforge.net/projects/peace-equalizer-apo-extension/files'
-    peace_equalizer_ver_xpath = '//table[@id="files_list"]/tbody/tr[7]/td[1]/abbr/text()'
-    peace_equalizer_ver_list = scrape_app_version(peace_equalizer_link, peace_equalizer_ver_xpath)
-    peace_equalizer_ver = peace_equalizer_ver_list[0]
-
-    print("Peace Equalizer: {}".format(peace_equalizer_ver))
-
-def search_everything_check():
-    search_everything_link = 'https://www.voidtools.com/downloads'
-    search_everything_ver_xpath = '//h2[@id="dl"]/text()'
-    search_everything_ver_list = scrape_app_version(search_everything_link, search_everything_ver_xpath)
-    search_everything_ver = search_everything_ver_list[0].split(' ')[-1]
-
-    print("Search Everything: {}".format(search_everything_ver))
-
-def exiftool_check():
-    exiftool_link = 'https://exiftool.org'
-    exiftool_ver_xpath = '//a[contains(text(),"exiftool-")]/text()'
-    exiftool_ver_list = scrape_app_version(exiftool_link, exiftool_ver_xpath)
-    exiftool_ver = exiftool_ver_list[0].split(' ')[-1].split('-')[-1][0:-4]
-
-    print("Exiftool: {}".format(exiftool_ver))
-
-def faststone_check():
-    faststone_link = 'https://www.faststone.org/FSIVDownload.htm'
-    faststone_ver_xpath = '//font[contains(text(),"FastStone Image Viewer")]/text()'
-    faststone_ver_list = scrape_app_version(faststone_link, faststone_ver_xpath)
-    faststone_ver = faststone_ver_list[-1].split(' ')[-2][0:3]
-
-    print("FastStone: {}".format(faststone_ver))
-
-def ffmpeg_check():
-    ffmpeg_link = 'https://ffmpeg.zeranoe.com/builds'
-    ffmpeg_ver_xpath = '//label[@class="btn btn-secondary active"]/text()'
-    ffmpeg_ver_list = scrape_app_version(ffmpeg_link, ffmpeg_ver_xpath)
-    ffmpeg_ver = ffmpeg_ver_list[1].strip()
-
-    print("FastStone: {}".format(ffmpeg_ver))
-
-def git_check():
-    git_link = 'https://git-scm.com/download/win'
-    git_ver_xpath = '//p[1]/strong[1]/text()'
-    git_ver_list = scrape_app_version(git_link, git_ver_xpath)
-    git_ver = git_ver_list[0]
-
-    print("Git: {}".format(git_ver))
-
-def greenshot_check():
-    greenshot_link = 'https://getgreenshot.org/downloads'
-    greenshot_ver_xpath = '//div[2]/div[1]/div[1]/text()'
-    greenshot_ver_list = scrape_app_version(greenshot_link, greenshot_ver_xpath)
-    greenshot_ver = greenshot_ver_list[2].strip().split('-')[-1]
-
-    print("Greenshot: {}".format(greenshot_ver))
-
-def hashtab_check():
-    hashtab_link = 'http://implbits.com/products/hashtab'
-    hashtab_ver_xpath = '//a[@id="btn_download_now"]'
-    hashtab_ver_list = scrape_app_version(hashtab_link, hashtab_ver_xpath)
-    hashtab_ver = hashtab_ver_list[0].get("href").split('_')[1][1:]
-
-    print("Hashtab: {}".format(hashtab_ver))
-
-def hwinfo_check():
-    hwinfo_link = 'https://www.hwinfo.com/download'
-    hwinfo_ver_xpath = '//sub/text()'
-    hwinfo_ver_list = scrape_app_version(hwinfo_link, hwinfo_ver_xpath)
-    hwinfo_ver = hwinfo_ver_list[0].split(' ')[-1]
-
-    print("HWInfo: {}".format(hwinfo_ver))
-
-def itunes_check():
-    itunes_link = 'https://en.wikipedia.org/wiki/ITunes'
-    itunes_ver_xpath = '//table[@class="infobox vevent"]/tbody/tr/td//text()'
-    itunes_ver_list = scrape_app_version(itunes_link, itunes_ver_xpath)
-    itunes_ver = itunes_ver_list[8].split(' ')[0].strip()
-
-    print("iTunes: {}".format(itunes_ver))
-
-def klitecodec_check():
-    klitecodec_link = 'https://codecguide.com/download_k-lite_codec_pack_standard.htm'
-    klitecodec_ver_xpath = '//h4/text()'
-    klitecodec_ver_list = scrape_app_version(klitecodec_link, klitecodec_ver_xpath)
-    klitecodec_ver = klitecodec_ver_list[0].split(' ')[1]
-
-    print("K-Lite Codec: {}".format(klitecodec_ver))
-
-def mkvtoolnix_check():
-    mkvtoolnix_link = 'https://www.fosshub.com/MKVToolNix.html'
-    mkvtoolnix_ver_xpath = '//dd[@itemprop="softwareVersion"]/text()'
-    mkvtoolnix_ver_list = scrape_app_version(mkvtoolnix_link, mkvtoolnix_ver_xpath)
-    mkvtoolnix_ver = mkvtoolnix_ver_list[0]
-
-    print("MKVToolnix: {}".format(mkvtoolnix_ver))
-
-def obs_check():
-    obs_link = 'https://obsproject.com/download'
-    obs_ver_xpath = '//span[@class="dl_ver"]/text()'
-    obs_ver_list = scrape_app_version(obs_link, obs_ver_xpath)
-    obs_ver = obs_ver_list[0].split(' ')[-1]
-
-    print("OBS: {}".format(obs_ver))
-
-def open_shell_check():
-    open_shell_link = 'https://github.com/Open-Shell/Open-Shell-Menu/releases'
-    open_shell_ver_xpath = '//div[@class="f1 flex-auto min-width-0 text-normal"]/a/text()'
-    open_shell_ver_list = scrape_app_version(open_shell_link, open_shell_ver_xpath)
-    open_shell_ver = open_shell_ver_list[0]
-
-    print("Open Shell: {}".format(open_shell_ver))
-
-def python_check():
-    python_link = 'https://www.python.org/downloads'
-    python_ver_xpath = '//a[@class="button"]/text()'
-    python_ver_list = scrape_app_version(python_link, python_ver_xpath)
-    python_ver = python_ver_list[0].split(' ')[-1]
-
-    print("Python: {}".format(python_ver))
-
-def rufus_check():
-    rufus_link = 'https://github.com/pbatard/rufus/releases/latest'
-    rufus_ver_xpath = '//div[@class="f1 flex-auto min-width-0 text-normal"]/a/text()'
-    rufus_ver_list = scrape_app_version(rufus_link, rufus_ver_xpath)
-    rufus_ver = rufus_ver_list[0].split(' ')[1]
-
-    print("Rufus: {}".format(rufus_ver))
-
-def sublime_check():
-    sublime_link = 'https://www.sublimetext.com/3dev'
-    sublime_ver_xpath = '//p[@class="latest"]/text()'
-    sublime_ver_list = scrape_app_version(sublime_link, sublime_ver_xpath)
-    sublime_ver = sublime_ver_list[0].split(' ')[-1]
-
-    print("Sublime Text: {}".format(sublime_ver))
-
-def visual_studio_code_check():
-    visual_studio_code_link = 'https://code.visualstudio.com/updates'
-    visual_studio_code_ver_xpath = '//div[@class="col-sm-9 col-md-8 body"]/h1/text()'
-    visual_studio_code_ver_list = scrape_app_version(visual_studio_code_link, visual_studio_code_ver_xpath)
-    visual_studio_code_ver = visual_studio_code_ver_list[0].split(' ')[-1][0:-1]
-
-    print("Visual Studio Code: {}".format(visual_studio_code_ver))
-
-def winscp_check():
-    winscp_link = 'https://winscp.net/eng/download.php'
-    winscp_ver_xpath = '//a[@class="btn btn-primary btn-lg"]/text()'
-    winscp_ver_list = scrape_app_version(winscp_link, winscp_ver_xpath)
-    winscp_ver = winscp_ver_list[1].split(' ')[1]
-
-    print("WinSCP: {}".format(winscp_ver))
-
-def g_music_desktop_check():
-    g_music_desktop_link = 'https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases'
-    g_music_desktop_ver_xpath = '//div[@class="f1 flex-auto min-width-0 text-normal"]/a/text()'
-    g_music_desktop_ver_list = scrape_app_version(g_music_desktop_link, g_music_desktop_ver_xpath)
-    g_music_desktop_ver = g_music_desktop_ver_list[0].split(' ')[-1]
-
-    print("Google Play Music Desktop: {}".format(g_music_desktop_ver))
-
-def java_check():
-    java_link = 'https://www.java.com/en/download/manual.jsp'
-    java_ver_xpath = '//h4[@class="sub"]/text()'
-    java_ver_list = scrape_app_version(java_link, java_ver_xpath)
-    java_ver = ' '.join(java_ver_list[0].split(' ')[1:])
-
-    print("Java: {}".format(java_ver))
-
+# import local copy with apps information
 def load_apps_info():
     apps_wb = openpyxl.load_workbook('./apps_info.xlsx')
     apps_sheet = apps_wb['Sheet1']
-    apps_dict = {}
+    apps_data = {}
 
     max_row_plus = apps_sheet.max_row + 1
 
@@ -283,14 +37,94 @@ def load_apps_info():
             }
         }
 
-        apps_dict.update(temp_dict)
+        apps_data.update(temp_dict)
 
-    print(apps_dict)
+    # print(apps_data['visual_c']['app_name'])
 
-    return apps_dict
+    return apps_data
+
+# check all apps to see for new updates
+def check_apps_version():
+    apps_dict = load_apps_info()
+    # print(apps_dict)
+
+    # specific syntax to capture the version string from different website
+    for key in apps_dict:
+        scrape_ver_list = scrape_app_version(apps_dict[key]['app_link'], apps_dict[key]['app_ver_xpath'])
+        if key == 'visual_c':
+            app_ver = scrape_ver_list[0][1:]
+        elif key == 'seven_zip':
+            app_ver = scrape_ver_list[0].split(' ')[2]
+        elif key == 'audacity':
+            app_ver = str(scrape_ver_list[0])
+        elif key == 'calibre':
+            app_ver = scrape_ver_list[1].split(' ')[1]
+        elif key == 'cmder':
+            app_ver = scrape_ver_list[0][1:]
+        elif key == 'crystaldiskinfo':
+            app_ver = scrape_ver_list[0].split(' ')[2]
+        elif key == 'crystaldiskmark':
+            app_ver = scrape_ver_list[0].split(' ')[3]
+        elif key == 'dropbox':
+            app_ver = scrape_ver_list[7].split(' ')[0]
+        elif key == 'equalizer_apo':
+            app_ver = scrape_ver_list[0]
+        elif key == 'peace_equalizer':
+            app_ver = scrape_ver_list[0]
+        elif key == 'search_everything':
+            app_ver = scrape_ver_list[0].split(' ')[-1]
+        elif key == 'exiftool':
+            app_ver = scrape_ver_list[0].split(' ')[-1].split('-')[-1][0:-4]
+        elif key == 'faststone':
+            app_ver = scrape_ver_list[-1].split(' ')[-2][0:3]
+        elif key == 'ffmpeg':
+            app_ver = scrape_ver_list[1].strip()
+        elif key == 'firefox':
+            app_ver = scrape_ver_list[21].split(' ')[0]
+        elif key == 'git':
+            app_ver = scrape_ver_list[0]
+        elif key == 'greenshot':
+            app_ver = scrape_ver_list[2].strip().split('-')[-1]
+        elif key == 'hashtab':
+            app_ver = scrape_ver_list[0].get("href").split('_')[1][1:]
+        elif key == 'hwinfo':
+            app_ver = scrape_ver_list[0].split(' ')[-1]
+        elif key == 'itunes':
+            app_ver = scrape_ver_list[8].split(' ')[0].strip()
+        elif key == 'klite_codec':
+            app_ver = scrape_ver_list[0].split(' ')[1]
+        elif key == 'mkvtoolnix':
+            app_ver = scrape_ver_list[0]
+        elif key == 'obs':
+            app_ver = scrape_ver_list[0].split(' ')[-1]
+        elif key == 'open_shell':
+            app_ver = scrape_ver_list[0]
+        elif key == 'sublime_text':
+            app_ver = scrape_ver_list[0].split(' ')[-1]
+        elif key == 'visual_studio_code':
+            app_ver = scrape_ver_list[0].split(' ')[-1][0:-1]
+        elif key == 'winscp':
+            app_ver = scrape_ver_list[1].split(' ')[1]
+        elif key == 'python':
+            app_ver = scrape_ver_list[0].split(' ')[-1]
+        elif key == 'rufus':
+            app_ver = scrape_ver_list[0].split(' ')[1]
+        elif key == 'g_play_music':
+            app_ver = scrape_ver_list[0].split(' ')[-1]
+        elif key == 'java':
+            app_ver = ' '.join(scrape_ver_list[0].split(' ')[1:])
+        else:
+            print("Something went wrong...")
+            break
+
+        # print("{}: {}".format(apps_dict[key]['app_name'], app_ver))
+
+        # check if there is a new app version
+        if app_ver != apps_dict[key]['app_local_version']:
+            print("{} is outdated! There is a new version: {}".format(apps_dict[key]['app_name'], app_ver))
 
 def main():
-    load_apps_info()
+    check_apps_version()
 
 if __name__ == '__main__':
     main()
